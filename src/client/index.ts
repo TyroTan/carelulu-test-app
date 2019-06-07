ba17e5688b1427110233a731f32a2acf76c07f92
ty <tantyrohunter@gmail.com>
2019-06-08 04:06:12 +0800

    Init graphql endpoint

--- src/client/index.ts
+++ src/client/index.ts
@@ -17,7 +17,7 @@ const client = new ApolloClient({
     }),
     new HttpLink({
       uri:
-        "http://localhost:4000/graphql",
+        "http://209.97.165.153:4000/graphql",
       credentials: "same-origin"
     })
   ]),
